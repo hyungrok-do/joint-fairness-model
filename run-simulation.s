c029463bf0b3953ca35f65029a5582434e0399aa
hyungrok-do <hyungrok-do@github.com>
2021-05-10 01:58:45 -0400

    minor updates

--- run-simulation.s
+++ run-simulation.s
@@ -8,20 +8,21 @@
 #SBATCH --output=./logs/JFM_%a.out
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=your@mail
-#SBATCH -a 0-1319
+#SBATCH -a 0-1759
 
 module load anaconda3/2020.07
 
 r_dict=(0 4 8 12 16 20 24 28 32 36 40)
 b_dict=(0 -2.5 -5 -7.5 -10 -12.5 -15 -17.5 -20 -22.5 -25)
 n_dict=(50 75 100 125 150 175 200 225 250 275 300)
+p_dict=(50 100 200 400 600 800 1000 1200 1400 1600 1800 2000)
 
 t_dict=(0 2 4 6 8 10 12 14 16 18 20)
 b_dict2=(0 2.5 5 7.5 10 12.5 15 17.5 20 22.5 25)
 n_dict2=(500 800 1100 1400 1700 2000 2300 2600 2900 3200 3500)
 
 
-for scenario in 0 1 2 3 4 5
+for scenario in 0 1 2 3 4 5 6 7
 do
     for seed in {0..19}
     do
@@ -66,24 +67,40 @@ then
             --n2 $n
 elif [ "$target_scenario" -eq 3 ]
 then
+    p=${p_dict[target_set_id]}
+    python experiments-simulation.py \
+            --name "sim4_${p}_${target_seed}" \
+            --seed $target_seed \
+            --p $p
+elif [ "$target_scenario" -eq 4 ]
+then
     t=${t_dict[target_set_id]}
     python experiments-simulation.py \
             --name "sim1b_${t}_${target_seed}" \
             --seed $target_seed \
             --r 0 \
             --t $t
-elif [ "$target_scenario" -eq 4 ]
+elif [ "$target_scenario" -eq 5 ]
 then
     b=${b_dict2[target_set_id]}
     python experiments-simulation.py \
             --name "sim2b_${b}_${target_seed}" \
             --seed $target_seed \
             --b $b
-elif [ "$target_scenario" -eq 5 ]
+elif [ "$target_scenario" -eq 6 ]
 then
     n=${n_dict2[target_set_id]}
     python experiments-simulation.py \
             --name "sim3b_${n}_${target_seed}" \
             --seed $target_seed \
             --n1 $n
+elif [ "$target_scenario" -eq 7 ]
+then
+    p=${p_dict[target_set_id]}
+    python experiments-simulation.py \
+            --name "sim4b_${p}_${target_seed}" \
+            --seed $target_seed \
+            --p $p \
+            --q $((p * 3 / 10)) \
+            --r $((p * 3 / 20))
 fi
--- run-simulation.sh
+++ run-simulation.sh
@@ -30,3 +30,61 @@ do
            --n2 $n
   done
 done
+
+for p in 50 100 200 400 600 800 1000 1200 1400 1600 1800 2000
+do
+  for seed in {0..19}
+  do
+    python experiments-simulation.py \
+           --name "sim4_${p}_${target_seed}" \
+           --seed $target_seed \
+           --p $p
+  done
+done
+
+for t in 0 2 4 6 8 10 12 14 16 18 20
+do
+  for seed in {0..19}
+  do
+    python experiments-simulation.py \
+            --name "sim1b_${t}_${seed}" \
+            --seed $seed \
+            --r 0 \
+            --t $t
+  done
+done
+
+for b in 0 2.5 5 7.5 10 12.5 15 17.5 20 22.5 25
+do
+  for seed in {0..19}
+  do
+    python experiments-simulation.py \
+            --name "sim2b_${b}_${seed}" \
+            --seed $seed \
+            --b $b
+  done
+done
+
+for n in 500 800 1100 1400 1700 2000 2300 2600 2900 3200 3500
+do
+  for seed in {0..19}
+  do
+    python experiments-simulation.py \
+           --name "sim3b_${n}_${target_seed}" \
+           --seed $target_seed \
+           --n1 $n
+  done
+done
+
+for p in 50 100 200 400 600 800 1000 1200 1400 1600 1800 2000
+do
+  for seed in {0..19}
+  do
+    python experiments-simulation.py \
+           --name "sim4b_${p}_${target_seed}" \
+           --seed $target_seed \
+            --p $p \
+            --q $((p * 3 / 10)) \
+            --r $((p * 3 / 20))
+  done
+done
